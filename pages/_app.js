5e8185e9ea0ce288f557a4ac84f0562f6cfdb72d
S-North <stuart.north@gmail.com>
2022-10-17 13:53:20 +0100

    Update _app.js
    Meta tags

--- pages/_app.js
+++ pages/_app.js
@@ -10,6 +10,9 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <link rel="shortcut icon" href="/images/shield.svg" />
         <title>Campaign Tracker</title>
+        <meta name="og:title" content="GM Tracker: Managing Your Campaign Made Easy"></meta>
+        <meta property="og:description" content="A new app for managing your campaigns, encounters, custom monsters and more"></meta>
+        <meta property="og:image" content="https://gm-tracker.netlify.app/images/bilgewater.jpg"></meta>
       </Head>
       <Layout>
         <Component {...pageProps} />
